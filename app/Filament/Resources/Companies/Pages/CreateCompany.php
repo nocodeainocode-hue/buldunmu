0791b3fbeaea5b2434b6a950b662ce466c24166f
nocodeainocode-hue <nocodeainocode@gmail.com>
2026-07-09 16:08:28 +0300

    fix: auto-set directory_id on company create

--- app/Filament/Resources/Companies/Pages/CreateCompany.php
+++ app/Filament/Resources/Companies/Pages/CreateCompany.php
@@ -4,8 +4,23 @@ namespace App\Filament\Resources\Companies\Pages;
 
 use App\Filament\Resources\Companies\CompanyResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateCompany extends CreateRecord
 {
     protected static string $resource = CompanyResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $dir = app()->bound('currentDirectory') ? app('currentDirectory') : null;
+        if ($dir) {
+            $data['directory_id'] = $dir->id;
+        }
+        return $data;
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        return static::getModel()::create($data);
+    }
 }
