d218710cf49687e94c56f15b79206478fe64c70f
nocodeainocode-hue <nocodeainocode@gmail.com>
2026-07-22 16:48:49 +0300

    fix(filament): use schemas Section component

--- app/Filament/Resources/MembershipPlans/Schemas/MembershipPlanForm.php
+++ app/Filament/Resources/MembershipPlans/Schemas/MembershipPlanForm.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources\MembershipPlans\Schemas;
 
 use App\Models\Directory;
 use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Section;
+use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
