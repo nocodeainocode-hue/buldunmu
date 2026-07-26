608ff0a1ae9f84728c749d6d000a40499c0ad69c
nocodeainocode-hue <nocodeainocode@gmail.com>
2026-07-26 18:23:40 +0300

    fix(categories): auto-assign directory_id when creating category from admin

--- app/Filament/Resources/Categories/Pages/CreateCategory.php
+++ app/Filament/Resources/Categories/Pages/CreateCategory.php
@@ -8,4 +8,13 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateCategory extends CreateRecord
 {
     protected static string $resource = CategoryResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $dir = app()->bound('currentDirectory') ? app('currentDirectory') : null;
+        if ($dir) {
+            $data['directory_id'] = $dir->id;
+        }
+        return $data;
+    }
 }
